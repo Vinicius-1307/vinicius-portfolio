bdef9075f7db472423c72477d61cae62cf3a4505
Vinicius-1307 <viniciusjose9@outlook.com>
2023-11-22 20:25:49 -0300

    refactor: responsive in header

--- app/components/HeaderComponent.tsx
+++ app/components/HeaderComponent.tsx
@@ -2,9 +2,9 @@ export default function Header() {
   return (
     <div className='w-full h-screen bg-[#0a192f] pl-5 md:px-72 flex items-center gap-10'>
       <div className='flex flex-col gap-2'>
-        <h1 className='font-mono text-[#64ffda] text-5xl md:text-lg text-center md:text-start'>Olá, meu nome é</h1>
-        <h2 className="font-sans font-bold text-7xl text-[#ccd6f6]">Vinicius José.</h2>
-        <h2 className="font-sans font-bold text-7xl text-[#8892b0]">Sou Desenvolvedor Web.</h2>
+        <h1 className='font-mono text-[#64ffda] text-lg md:text-lg  text-start'>Olá, meu nome é</h1>
+        <h2 className="font-sans font-bold text-4xl md:text-7xl text-[#ccd6f6]">Vinicius José.</h2>
+        <h2 className="font-sans font-bold text-3xl md:text-7xl text-[#8892b0]">Sou Desenvolvedor Web.</h2>
         <p className="max-w-[500px] text-[#8892b0]">Sou desenvolvedor de sistemas, atualmente como estagiário. Atualmente meu foco está volado para o back-end, onde eu realmente me encontro.</p>
       </div>
     </div>
