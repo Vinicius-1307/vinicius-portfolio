ccc947900e9495901df4186285b2823f9f7676c7
Vinicius-1307 <viniciusjose9@outlook.com>
2023-11-22 19:47:18 -0300

    feat: created email component

---
+++ app/components/EmailComponent.tsx
@@ -0,0 +1,14 @@
+import { Icon } from "@iconify/react";
+
+export default function EmailComponent(){
+  return (
+    <div className='fixed flex flex-col gap-6 top-[500px] right-20'>
+        <div className="flex flex-col items-center">
+          <a href="https://www.instagram.com/vinicius.j10/" target='_blank'>
+            <p className="font-mono text-[#8892b0] rotate-90 mb-32">viniciusjose9@outlook.com</p>
+          </a>
+          <div className="border-l border-[#8892b0] h-56"></div>
+        </div>
+    </div>
+  )
+}
