cd9445ce2db5dda19c2358c20c53f3f08608122c
Vinicius-1307 <viniciusjose9@outlook.com>
2023-11-22 19:47:47 -0300

    feat: implemented new components in home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,20 @@ import MyKnowAges from './components/MyKnowAges';
 import Header from './components/HeaderComponent';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import SocialMediaComponent from './components/SocialMediaComponent';
+import EmailComponent from './components/EmailComponent';
 
 export default function Home() {
 
   return (
-    <main className="min-h-screen">
-        {/* <Navbar/> */}
-        <Header/>
-        <MyKnowAges/>
-        <AboutComponent/>
-        <Footer/>
-    </main >
+    <main className="min-h-screen relative">
+      <SocialMediaComponent/>
+      <EmailComponent/>
+      {/* <Navbar/> */}
+      <Header/>
+      <MyKnowAges/>
+      <AboutComponent/>
+      <Footer/>
+    </main>
   )
 }
